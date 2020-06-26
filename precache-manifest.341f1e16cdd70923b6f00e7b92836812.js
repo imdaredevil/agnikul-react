8053c4896acfe83a4d977a6d75454bda39dd9733
cibi <cibi_16@live.com>
2020-06-26 19:53:17 +0530

    Updates

---
+++ precache-manifest.341f1e16cdd70923b6f00e7b92836812.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e9b1efa26888602cf5b4b948f5b22f",
+    "url": "/agnikul-react/index.html"
+  },
+  {
+    "revision": "bd515d8b9dc414ff1416",
+    "url": "/agnikul-react/static/css/10.c24cc946.chunk.css"
+  },
+  {
+    "revision": "e3dd56746f6f1baa7637",
+    "url": "/agnikul-react/static/css/11.9be4f162.chunk.css"
+  },
+  {
+    "revision": "d6a02a9367a5706039fa",
+    "url": "/agnikul-react/static/css/3.58940346.chunk.css"
+  },
+  {
+    "revision": "6c8e5142cf9856cd259f",
+    "url": "/agnikul-react/static/css/4.f13bd256.chunk.css"
+  },
+  {
+    "revision": "605483ac210c9f70b17c",
+    "url": "/agnikul-react/static/css/5.912ceb2b.chunk.css"
+  },
+  {
+    "revision": "39a3f9907762f04b5ef4",
+    "url": "/agnikul-react/static/css/6.555f0aa6.chunk.css"
+  },
+  {
+    "revision": "046a61a7f02d457999e9",
+    "url": "/agnikul-react/static/css/7.1b4560b1.chunk.css"
+  },
+  {
+    "revision": "b3a7b7bf799326042915",
+    "url": "/agnikul-react/static/css/8.a1563784.chunk.css"
+  },
+  {
+    "revision": "9e686b9ae42be1c9b300",
+    "url": "/agnikul-react/static/css/9.1e003bd0.chunk.css"
+  },
+  {
+    "revision": "18bbd8c84b5074a72b01",
+    "url": "/agnikul-react/static/css/main.f722004f.chunk.css"
+  },
+  {
+    "revision": "3f6a39a48ac8a036ab75",
+    "url": "/agnikul-react/static/js/0.deaae401.chunk.js"
+  },
+  {
+    "revision": "97c7eb2bcee0a6fc840a51ef54eeeb58",
+    "url": "/agnikul-react/static/js/0.deaae401.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd515d8b9dc414ff1416",
+    "url": "/agnikul-react/static/js/10.debfe714.chunk.js"
+  },
+  {
+    "revision": "e3dd56746f6f1baa7637",
+    "url": "/agnikul-react/static/js/11.7a682cb1.chunk.js"
+  },
+  {
+    "revision": "d6a02a9367a5706039fa",
+    "url": "/agnikul-react/static/js/3.770fac7e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/agnikul-react/static/js/3.770fac7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c8e5142cf9856cd259f",
+    "url": "/agnikul-react/static/js/4.b6410ccd.chunk.js"
+  },
+  {
+    "revision": "605483ac210c9f70b17c",
+    "url": "/agnikul-react/static/js/5.30236b50.chunk.js"
+  },
+  {
+    "revision": "39a3f9907762f04b5ef4",
+    "url": "/agnikul-react/static/js/6.908e1561.chunk.js"
+  },
+  {
+    "revision": "046a61a7f02d457999e9",
+    "url": "/agnikul-react/static/js/7.27d3604c.chunk.js"
+  },
+  {
+    "revision": "b3a7b7bf799326042915",
+    "url": "/agnikul-react/static/js/8.67f2f82d.chunk.js"
+  },
+  {
+    "revision": "9e686b9ae42be1c9b300",
+    "url": "/agnikul-react/static/js/9.08313fe1.chunk.js"
+  },
+  {
+    "revision": "18bbd8c84b5074a72b01",
+    "url": "/agnikul-react/static/js/main.5158953a.chunk.js"
+  },
+  {
+    "revision": "fa908c0ee9260f39922a",
+    "url": "/agnikul-react/static/js/runtime-main.6de8bc8c.js"
+  },
+  {
+    "revision": "49882afbfc3f126ca6a05783b400669e",
+    "url": "/agnikul-react/static/media/Agnibaan.49882afb.png"
+  },
+  {
+    "revision": "58912875dfd3a5037509d9fdb93fb1b5",
+    "url": "/agnikul-react/static/media/Launchpad_V6.58912875.png"
+  },
+  {
+    "revision": "9c04694488e555c0cdf3b3008d5a71c6",
+    "url": "/agnikul-react/static/media/Poppins-Black.9c046944.woff"
+  },
+  {
+    "revision": "c9c9155e22db6189a94ab55ded95678e",
+    "url": "/agnikul-react/static/media/Poppins-BlackItalic.c9c9155e.woff"
+  },
+  {
+    "revision": "7ed36eb07742be0ec902d7335574fe7b",
+    "url": "/agnikul-react/static/media/Poppins-Bold.7ed36eb0.woff"
+  },
+  {
+    "revision": "f0308f4ba2f35e851c60842504b5167a",
+    "url": "/agnikul-react/static/media/Poppins-BoldItalic.f0308f4b.woff"
+  },
+  {
+    "revision": "cf36ad64c82287335e90cd4597446ac9",
+    "url": "/agnikul-react/static/media/Poppins-ExtraBold.cf36ad64.woff"
+  },
+  {
+    "revision": "423de4fe76076d809e3277f7300acb5c",
+    "url": "/agnikul-react/static/media/Poppins-ExtraBoldItalic.423de4fe.woff"
+  },
+  {
+    "revision": "bd77f77be3c46095a923f5d3f6ec1b77",
+    "url": "/agnikul-react/static/media/Poppins-ExtraLight.bd77f77b.woff"
+  },
+  {
+    "revision": "d6afbe9cfab5b3079db9a865a2053acb",
+    "url": "/agnikul-react/static/media/Poppins-ExtraLightItalic.d6afbe9c.woff"
+  },
+  {
+    "revision": "27f17315803ff82ff44214018590df7c",
+    "url": "/agnikul-react/static/media/Poppins-Italic.27f17315.woff"
+  },
+  {
+    "revision": "843f47805ba6f6dd82fc06b728b6b001",
+    "url": "/agnikul-react/static/media/Poppins-Light.843f4780.woff"
+  },
+  {
+    "revision": "709385f16c58cbc7be5f60ae03b19426",
+    "url": "/agnikul-react/static/media/Poppins-LightItalic.709385f1.woff"
+  },
+  {
+    "revision": "4a53ae46ee7019764d72058deb865863",
+    "url": "/agnikul-react/static/media/Poppins-Medium.4a53ae46.woff"
+  },
+  {
+    "revision": "0d317bdea04b10795b2b0b23b59b16dc",
+    "url": "/agnikul-react/static/media/Poppins-MediumItalic.0d317bde.woff"
+  },
+  {
+    "revision": "98a10b7edfafa2f4ad52cfba378a9390",
+    "url": "/agnikul-react/static/media/Poppins-Regular.98a10b7e.woff"
+  },
+  {
+    "revision": "d02649ad9e384e49029ca59486f02aa9",
+    "url": "/agnikul-react/static/media/Poppins-SemiBold.d02649ad.woff"
+  },
+  {
+    "revision": "6dc40c6b1ba1a674ba342d86002f5658",
+    "url": "/agnikul-react/static/media/Poppins-SemiBoldItalic.6dc40c6b.woff"
+  },
+  {
+    "revision": "4523bd917c3744602247bb858d098678",
+    "url": "/agnikul-react/static/media/Poppins-Thin.4523bd91.woff"
+  },
+  {
+    "revision": "7a314397278cf5a4574be15f951517b1",
+    "url": "/agnikul-react/static/media/Poppins-ThinItalic.7a314397.woff"
+  },
+  {
+    "revision": "18ed86fe6b5700e53115b3732da9aea7",
+    "url": "/agnikul-react/static/media/bitmap.18ed86fe.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/agnikul-react/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/agnikul-react/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/agnikul-react/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/agnikul-react/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/agnikul-react/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "985b1987d3cb2f67506bcbcb07926dc1",
+    "url": "/agnikul-react/static/media/group-14.985b1987.png"
+  },
+  {
+    "revision": "d54b04b923e019377eaba47e24c3335f",
+    "url": "/agnikul-react/static/media/group-17.d54b04b9.png"
+  },
+  {
+    "revision": "c4be848c5accd4d6bbb106bfa3c8b41a",
+    "url": "/agnikul-react/static/media/group-9.c4be848c.png"
+  },
+  {
+    "revision": "91fad139249bb835902f106357d10d95",
+    "url": "/agnikul-react/static/media/homepage.91fad139.jpeg"
+  },
+  {
+    "revision": "6463c17c772ee4fec074cd7f36b1c3fe",
+    "url": "/agnikul-react/static/media/logo.6463c17c.png"
+  },
+  {
+    "revision": "0f98bd0ef008db197ad881391818c041",
+    "url": "/agnikul-react/static/media/rocket-1.0f98bd0e.png"
+  },
+  {
+    "revision": "f9d9bb3661c2f1ff60ca24341122ffbd",
+    "url": "/agnikul-react/static/media/rocket-2.f9d9bb36.png"
+  },
+  {
+    "revision": "aac3c5eed3ede6770308a9d94c0995c0",
+    "url": "/agnikul-react/static/media/rocket-3.aac3c5ee.png"
+  },
+  {
+    "revision": "cc275313e2bdf3c4ba788339dd2c718b",
+    "url": "/agnikul-react/static/media/spacex-logo.cc275313.png"
+  },
+  {
+    "revision": "99cbf9e6f51de0203148c2d9af14b164",
+    "url": "/agnikul-react/static/media/teampage.99cbf9e6.jpg"
+  }
+]);
